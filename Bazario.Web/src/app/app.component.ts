be4485e157e6c7bb373e8604f3643218fba97f9c
Даня Краснопір <krasnopd@gmail.com>
2025-04-03 14:24:59 +0200

    Create input component

--- Bazario.Web/src/app/app.component.ts
+++ Bazario.Web/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { InputComponent } from "./shared/components/input/input.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterModule, InputComponent],
 })
 export class AppComponent {
 }
