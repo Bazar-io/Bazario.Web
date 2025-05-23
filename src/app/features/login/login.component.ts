57266b29aab6454fb0e5a281304081875a036d65
clowd1e <sashapro712@gmail.com>
2025-05-23 18:18:17 +0200

    Update login page

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { AuthInputComponent } from "../../shared/components/auth/auth-input/auth-input.component";
 import { AuthButtonComponent } from "../../shared/components/auth/auth-button/auth-button.component";
 import { AuthPasswordInputComponent } from "../../shared/components/auth/auth-password-input/auth-password-input.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   encapsulation: ViewEncapsulation.None,
-  imports: [AuthInputComponent, AuthButtonComponent, AuthPasswordInputComponent],
+  imports: [AuthInputComponent, RouterLink, AuthButtonComponent, AuthPasswordInputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
