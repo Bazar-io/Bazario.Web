c14af780d67e7d23aa52c7f519a1719d3755c4ee
clowd1e <sashapro712@gmail.com>
2025-05-23 18:18:43 +0200

    Add register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
 
 export const routes: Routes = [
     {
         'path': 'login',
         'component': LoginComponent,
+        'pathMatch': 'full'
+    },
+    {
+        'path': 'register',
+        'component': RegisterComponent,
         'pathMatch': 'full'
     }
 ];
