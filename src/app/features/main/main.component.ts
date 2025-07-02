d20d8056720d9394c8021bf085ec0e6f90df7dc0
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-07-02 10:57:51 +0200

    [BZRIO-148] Implement main page (#7)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-148
    * Install ngx-infinite-scroll and ngx-spinner
    * Remove gap between header and main content on smaller devices
    * Add main-search-bar component
    * Add ad-element component
    * Add ads-list component
    * Implement main page component

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MainSearchBarComponent } from './components/main-search-bar/main-search-bar.component';
+import { AdsListComponent } from "./components/ads-list/ads-list.component";
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [MainSearchBarComponent, AdsListComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
 }
---
+++ src/app/features/main/models/ad.ts
@@ -0,0 +1,7 @@
+export type Ad = {
+    id: string;
+    image: string;
+    description: string;
+    price: string;
+    refreshed: string;
+}
