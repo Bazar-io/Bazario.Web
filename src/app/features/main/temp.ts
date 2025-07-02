a03b12bcb15d715462b00ecf161cc3e6c653ea60
clowd1e <sashapro712@gmail.com>
2025-07-02 10:56:26 +0200

    Implement main page component

---
+++ src/app/features/main/temp.ts
@@ -0,0 +1,193 @@
+import { Ad } from "./models/ad";
+
+export const ads: Ad[] = [
+    {
+      id: '1',
+      image: 'https://i.pinimg.com/736x/31/94/de/3194de38c1ba816a5100b2d4e426c77d.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '2',
+      image: 'https://i.pinimg.com/736x/c1/51/36/c15136daed94decf33c4533372cb4395.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '3',
+      image: 'https://i.pinimg.com/736x/01/57/24/01572449d14acd8f33966c4dacd7ba82.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '4',
+      image: 'https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-09/220928-maga-hat-mjf-0936-0e037d.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '5',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM-fiIwhHM29ww0d4AaCwHFUETmHZ71ZyMFQ&s',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '6',
+      image: 'https://dixy.ru/upload/iblock/5fb/wohqvtevm6zio2m5k0yf0vpy27flavqt.webp',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '7',
+      image: 'https://i.pinimg.com/736x/be/3b/71/be3b71d9f58473a0be61d372e4906f29.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '8',
+      image: 'https://skin.vimeworld.com/helm/SosiskaKiller.png',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '9',
+      image: 'https://media.istockphoto.com/id/610041376/photo/beautiful-sunrise-over-the-sea.jpg?s=612x612&w=0&k=20&c=R3Tcc6HKc1ixPrBc7qXvXFCicm8jLMMlT99MfmchLNA=',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '10',
+      image: 'https://media.istockphoto.com/id/610041376/photo/beautiful-sunrise-over-the-sea.jpg?s=612x612&w=0&k=20&c=R3Tcc6HKc1ixPrBc7qXvXFCicm8jLMMlT99MfmchLNA=',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '11',
+      image: 'https://media.istockphoto.com/id/610041376/photo/beautiful-sunrise-over-the-sea.jpg?s=612x612&w=0&k=20&c=R3Tcc6HKc1ixPrBc7qXvXFCicm8jLMMlT99MfmchLNA=',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '12',
+      image: 'https://media.istockphoto.com/id/610041376/photo/beautiful-sunrise-over-the-sea.jpg?s=612x612&w=0&k=20&c=R3Tcc6HKc1ixPrBc7qXvXFCicm8jLMMlT99MfmchLNA=',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '13',
+      image: 'https://media.tenor.com/l9-P033MdWcAAAAe/%D1%82%D1%8B%D0%BA%D0%B2%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5-%D1%81%D0%B5%D0%BC%D0%B5%D1%87%D0%BA%D0%B8-%D1%82%D1%8B%D0%BA%D0%B2%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5.png',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '14',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRK7_ds7u-yobgM5BNveFt6laTbB_f8KNrPA&s',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '15',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs5IIr9t70dq8b3ctDhu4w-gKX4wrfULQHwA&s',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '300 $',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '16',
+      image: 'https://i1.sndcdn.com/artworks-RLGThjV8tJ70fVfc-uBP87w-t500x500.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '17',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5rQqE3W4XHVr6TzaDTWqjrb2RahJNmpT6kQ&s',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '18',
+      image: 'https://i.pinimg.com/736x/2d/ed/5a/2ded5a2ab1c490724f83c6474a668875.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '19',
+      image: 'https://i.pinimg.com/736x/4c/8d/af/4c8daf92ef29a230bc02b93d12e4cb0b.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '20',
+      image: 'https://i.pinimg.com/736x/40/72/e5/4072e5d7258c12687720048d9d82b440.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '1 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '21',
+      image: 'https://i.pinimg.com/736x/d1/fc/e7/d1fce75b0741cf42929ef82cc2f9f344.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '22',
+      image: 'https://i.pinimg.com/736x/9a/9d/eb/9a9deb9e4727a5e746a71bf71b465968.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '23',
+      image: 'https://i.pinimg.com/736x/7f/04/28/7f04287769d0b49eea2eb74a4c10d3eb.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '24',
+      image: 'https://i.pinimg.com/736x/7f/c8/38/7fc8386f0c88de03b12278d8c7b5e161.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '25',
+      image: 'https://i.pinimg.com/736x/13/82/d3/1382d3aee999ec78f5089a034c9ffba1.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '26',
+      image: 'https://i.pinimg.com/736x/5e/65/9e/5e659e44c3351ff468d7f0ebe2604291.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    },
+    {
+      id: '27',
+      image: 'https://i.pinimg.com/736x/78/16/c5/7816c57a382c96a1a0b4b53b08b7fb87.jpg',
+      description: 'Realme 14 Pro+ to smartfon, który zrewolucjonizuje Twoje podejście do fotografii. Oferuje aparat peryskopowy z 3-krotnym zoomem optycznym, zapewniający niezwykłą ostrość i detale nawet w trudnych warunkach oświetleniowych. Dzięki matrycy Sony IMX882 i optycznej stabilizacji obrazu (OIS) uchwycisz każdy szczegół, a główny aparat 50 MP (Sony IMX896) zagwarantuje wysoką jakość zdjęć zarówno w dzień, jak i w nocy.',
+      price: '100 zł',
+      refreshed: 'Odświeżono 15 kwietnia 2025'
+    }
+]
